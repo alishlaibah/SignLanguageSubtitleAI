d5cced4da470ef6540344f69333f0fdd31667102
Ali Shlaibah <shlaibaha@gmail.com>
2025-08-01 18:56:05 -0500

    Typos - Still doesn't work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function App() {
     return (
-        <div>
+        <div style={{textAlign: "center", backgroundColor: "black", color: "white"}}>
             <h1>Sign Language Subtitle AI MVP</h1>
-            <p>Hello!</p>
+            <p>Hello paragraph!</p>
         </div>
-    )
+    );
 }
 
 export default App;
