10f9364bfa86e1afb84222498081bce9c740ad79
Ali Shlaibah <shlaibaha@gmail.com>
2025-08-01 18:38:21 -0500

    Trying to get React and Vite to work locally

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>Sign Language Subtitle AI MVP</h1>
+            <p>Hello!</p>
+        </div>
+    )
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const root = ReactDOM.createROOT(document.getElementById('root'));
+
+root.render(<App />);
