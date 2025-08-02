3b83883386469b2860877c961ed7990cedafd0dc
Ali Shlaibah <shlaibaha@gmail.com>
2025-08-01 19:01:43 -0500

    Fixed Directory issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
     return (
         <div style={{textAlign: "center", backgroundColor: "black", color: "white"}}>
             <h1>Sign Language Subtitle AI MVP</h1>
-            <p>Hello paragraph!</p>
+            <p>sdalf!</p>
         </div>
     );
 }
